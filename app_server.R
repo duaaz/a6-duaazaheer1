fee5746fc5c7de7af768844d477d3d5bb73cf26f
Duaa Zaheer <duaazaheer@gmail.com>
2020-05-30 14:15:42 -0700

    final push

--- app_server.R
+++ app_server.R
@@ -1,3 +1,28 @@
 server <- function(input, output) {
-  
+  output$density_vs_poverty <- renderPlotly({
+    plot <- ggplot(
+      data = midwest,
+      aes(y = percbelowpoverty, x = popdensity)
+    )
+    plot + geom_point((aes(color = state))) +
+      ggtitle("Population Density Vs Total Poverty") +
+      xlab("Population Density") +
+      ylab("Percentage of People Living Below Poverty") +
+      scale_x_continuous(limits = c(input$xvalmin, input$xvalmax))
+  })
+  output$college_vs_poverty <- renderPlotly({
+    plot <- ggplot(data = midwest, aes(y = percbelowpoverty, x = percollege))
+    plot +
+      geom_point((aes(color = state))) +
+      ggtitle("College Education Vs Total Poverty") +
+      xlab("Percent College Educated") +
+      ylab("Total Poverty Percentage") +
+      scale_x_continuous(limits = c(input$xmin, input$xmax))
+  })
 }
+
+# styling
+library(lintr)
+library(styler)
+style_file("app_server.R")
+lint("app_server.R")
--- app_ui.R
+++ app_ui.R
@@ -1,30 +1,65 @@
 visualization_1 <- tabPanel(
-  "Visualization #1", 
+  "Visualization #1",
   sidebarLayout(
     sidebarPanel(
-      h2("Controllers are here")
-    ), 
+      h3("Changing the x-axis scale minimum"),
+      numericInput("xvalmin",
+        label = "x-axis minimum", value = 0, min = 0,
+        max = 100000
+      ),
+      h3("Changing the x-axis scale maximum"),
+      numericInput("xvalmax",
+        label = "x-axis maximum", value = 100000,
+        min = 0, max = 100000
+      )
+    ),
     mainPanel(
-      h1("Visualization name here")
+      h1("Population and Poverty"),
+      plotlyOutput(
+        outputId = "density_vs_poverty"
+      )
     )
   )
-  
 )
 
 visualization_2 <- tabPanel(
-  "Visualization #2", 
+  "Visualization #2",
   sidebarLayout(
     sidebarPanel(
-      h2("Controllers are here")
-    ), 
+      h3("Changing the x-axis scale minimum"),
+      sliderInput("xmin",
+        label = "x-minimum", min = 0, max = 50, value = 0, step = NULL,
+        round = FALSE, ticks = TRUE,
+        animate = FALSE, width = NULL, sep = ",", pre = NULL,
+        post = NULL, timeFormat = NULL, timezone = NULL,
+        dragRange = TRUE
+      ),
+      h3("Changing the x-axis scale maximum"),
+      sliderInput("xmax",
+        label = "x-maximum", min = 0, max = 50, value = 50, step = NULL,
+        round = FALSE, ticks = TRUE,
+        animate = FALSE, width = NULL, sep = ",", pre = NULL,
+        post = NULL, timeFormat = NULL, timezone = NULL,
+        dragRange = TRUE
+      ),
+    ),
     mainPanel(
-      h1("Visualization name here")
+      h1("College Education and Poverty"),
+      plotlyOutput(
+        outputId = "college_vs_poverty"
+      )
     )
   )
 )
 
 ui <- navbarPage(
   "USA Midwest County Demographics and Data Analysis",
-  visualization_1, 
+  visualization_1,
   visualization_2
 )
+
+# styling
+library(lintr)
+library(styler)
+style_file("app_ui.R")
+lint("app_ui.R")
